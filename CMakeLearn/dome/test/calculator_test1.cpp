a810951e634a293efc91d2277842aa32906ea888
Wang Haiyang <aythsr@yeah.net>
2025-03-09 23:57:32 +0800

    1

---
+++ CMakeLearn/dome/test/calculator_test1.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "mylib/mylib.h"
+#include <iostream>
+
+using namespace mylib;
+
+TEST(CalculatorTest, Add) {
+    Calculator calculator;
+    int result = calculator.Add(3, 3);
+    EXPECT_EQ(result, 5);
+}
+
+int main(int argc, char **argv) {
+    std::cout << argv[1] << ' ' << argv[2] << std::endl;
+    // 运行所有测试用例
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
