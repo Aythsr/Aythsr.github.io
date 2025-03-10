719e552eee34ca9e9a4f8fcdfeaf8413221d2366
Wang Haiyang <aythsr@yeah.net>
2025-03-10 21:03:49 +0800

    dome

--- CMakeLearn/dome/test/ttest.cc
+++ CMakeLearn/dome/test/ttest.cc
@@ -2,8 +2,11 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        return 99;
+        std::cout << "Usage: " << argv[0] << " <number>" << std::endl;
+        return 1;
     }
-    int a = atoi(argv[1]);
+    int a;
+    a = atoi(argv[1]);
+    std::cout << a << std::endl;
     return a;
 }
